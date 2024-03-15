b1dcc787dd0ccc1ac87272b714007b7b077e11e3
benguajardo <ben.guajardo@duocuc.cl>
2024-03-15 15:38:36 -0300

    segundo commit

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
         path('', index, name="index"),
         path('shop', shop, name="shop"),
+        path('base', base, name="base"),
+        path('detail', detail, name="detail"),
+        path('contact', contact, name="contact"),
+        path('checkout', checkout, name="checkout"),
+        path('cart', cart, name="cart"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,13 @@ def index(request):
     return render(request, 'core/index.html')
 def shop(request):
     return render(request, 'core/shop.html')
+def base(request):
+    return render(request, 'core/base.html')
+def detail(request):
+    return render(request, 'core/detail.html')
+def contact(request):
+    return render(request, 'core/contact.html')
+def checkout(request):
+    return render(request, 'core/checkout.html')
+def cart(request):
+    return render(request, 'core/cart.html')
